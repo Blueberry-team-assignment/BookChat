459a660e55a2e6176915b17429b99be4f16e5f4d
Haerin Kim <lini1634@naver.com>
2024-12-13 22:01:41 +0900

    modify BookSerializer

--- bookchat/serializers.py
+++ bookchat/serializers.py
@@ -2,7 +2,11 @@ from rest_framework import serializers
 from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
-    poster_url = serializers.SerializerMethodField()  
+    poster_url = serializers.SerializerMethodField()
+    
     class Meta:
         model = Book 
-        fields = ('title', 'keyword', 'poster', 'like')
+        fields = ('title', 'keyword', 'poster_url', 'like')  # id 필드 추가
+        
+    def get_poster_url(self, obj):
+        return obj.get_poster_url()
