2fe8c5b06c1297d8bea181f36a96922cbd6e3fde
Haerin Kim <lini1634@naver.com>
2024-12-14 00:15:11 +0900

    Add memo feature

--- bookchat/views.py
+++ bookchat/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .models import Book
+from .models import Book, BookMemo
 from .serializers import BookSerializer
 import random 
 
