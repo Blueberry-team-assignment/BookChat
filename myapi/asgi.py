7562634c07cc1883153dd9cc3813f4ba58434ef0
Haerin Kim <lini1634@naver.com>
2024-12-15 18:33:03 +0900

    asgi.py django.setup()

--- myapi/asgi.py
+++ myapi/asgi.py
@@ -8,12 +8,15 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from df_chat.asgi.routing import websocket_urlpatterns 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myapi.settings')
+django.setup()  # 추가: Django 앱 레지스트리를 명시적으로 초기화
+
+from df_chat.asgi.routing import websocket_urlpatterns  # django.setup() 이후로 이동
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
