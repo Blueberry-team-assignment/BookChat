fe3fa0bc290dd855ce0fae5810dc8123fbf972ff
Haerin Kim <lini1634@naver.com>
2024-12-17 15:51:30 +0900

    add get all books

--- bookchat/urls.py
+++ bookchat/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import helloAPI, randomBook, save_memo, get_memo, create_book
+from .views import helloAPI, randomBook, save_memo, get_memo, allBooks
 
 urlpatterns = [
     path("hello/", helloAPI),
     path("<int:id>/", randomBook),
     path("memo/", save_memo),
     path("memo/<int:book_id>/", get_memo),
-    path("books/", create_book)
+    path("books/", allBooks)
 ]
--- bookchat/views.py
+++ bookchat/views.py
@@ -15,6 +15,12 @@ def randomBook(request, id):
     serializer = BookSerializer(randomBooks, many=True)
     return Response(serializer.data)
 
+@api_view(['GET'])
+def allBooks(request):
+    totalBooks = Book.objects.all()
+    serializer = BookSerializer(totalBooks, many=True)
+    return Response(serializer.data)
+
 @api_view(['POST'])
 def save_memo(request):
     try:
@@ -68,6 +74,8 @@ def get_memo(request, book_id):
         return Response({'content': ''})  # 메모가 없는 경우 빈 내용 반환
     except Exception as e:
         return Response({'error': str(e)}, status=500)
+
+
 
 ####################################
 @api_view(['POST'])
