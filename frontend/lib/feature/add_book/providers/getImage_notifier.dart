0e3998990bde4297bf2131257400ac3028dd7db9
Haerin Kim <lini1634@naver.com>
2025-02-11 06:17:21 +0900

    이미지 압축 함수만 가져옴

--- frontend/lib/feature/add_book/providers/getImage_notifier.dart
+++ frontend/lib/feature/add_book/providers/getImage_notifier.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:book_chat/common/repository/gallery_repository.dart';
 import 'package:book_chat/models/addbook_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image/image.dart' as img;
 
 final getImageProvider = StateNotifierProvider<GetImageNotifier, AddBookState>((ref){
   final galleryRepository = ref.read(GalleryRepositoryProvider);
@@ -37,6 +40,22 @@ class GetImageNotifier extends StateNotifier<AddBookState>{
 
       updateAddBookDto(updatedDto);
     }
+  }
+
+  /* 이미지 압축 메서드 */
+  Future<File> _compressImage(File imageFile) async {
+    final bytes = await imageFile.readAsBytes();
+    img.Image image = img.decodeImage(bytes)!;
+
+    // 이미지 크기를 조정 (예: 너비를 1024로 설정)
+    img.Image resizedImage = img.copyResize(image, width: 800, height: 1200);
+
+    // 압축된 이미지 파일 생성 (JPEG 품질을 85로 설정)
+    final compressedBytes = img.encodeJpg(resizedImage, quality: 85);
+    final compressedImageFile = File('${imageFile.path}_compressed.jpg')
+      ..writeAsBytesSync(compressedBytes);
+
+    return compressedImageFile;
   }
 }
 
